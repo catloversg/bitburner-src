76cf2278ca65480674b309e41f9f756c618a2dd4
CatLover <152669316+catloversg@users.noreply.github.com>
2023-12-20 16:23:04 +0700

    Fix problem with pull request #972

--- custom-tools/saveFileEditor.ts
+++ custom-tools/saveFileEditor.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import { Engine } from "../src/engine";
 import { saveObject } from "../src/SaveObject";
 import { Player } from "../src/Player";
--- src/utils/Protections.ts
+++ src/utils/Protections.ts
@@ -2,8 +2,10 @@
 /* Prevent inadvertantly redefining certain window properties,
    which are known to cause unrecoverable game errors when redefined.
    The player is able to redefine these properties as writable if desired. */
-Object.defineProperties(window, {
-  Number: { writable: false },
-  Object: { writable: false },
-  String: { writable: false },
-});
+if (globalThis.window) {
+  Object.defineProperties(window, {
+    Number: { writable: false },
+    Object: { writable: false },
+    String: { writable: false },
+  });
+}
